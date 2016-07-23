a8caf871eadb7b77cdd78fa61f68f587617dd715
sawaryot <ryotasawa12smile@gmail.com>
2016-07-23 23:33:03 +0900

    ios側実行できるようにした

--- iOS/ViewController.designer.cs
+++ iOS/ViewController.designer.cs
@@ -11,45 +11,42 @@ using UIKit;
 
 namespace testApp.iOS
 {
-    [Register ("ViewController")]
-    partial class ViewController
-    {
-        [Outlet]
-        UIKit.UIButton Button { get; set; }
+	[Register ("ViewController")]
+	partial class ViewController
+	{
+		[Outlet]
+		UIKit.UIButton Button { get; set; }
 
-        [Outlet]
-        [GeneratedCode ("iOS Designer", "1.0")]
-        UIKit.UIButton CallButton { get; set; }
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIButton CallButton { get; set; }
 
-        [Outlet]
-        [GeneratedCode ("iOS Designer", "1.0")]
-        UIKit.UITextField PhoneNumberText { get; set; }
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UITextField PhoneNumberText { get; set; }
 
-        [Outlet]
-        [GeneratedCode ("iOS Designer", "1.0")]
-        UIKit.UIButton TranslateButton { get; set; }
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIButton TranslateButton { get; set; }
 
-        void ReleaseDesignerOutlets ()
-        {
-            if (Button != null) {
-                Button.Dispose ();
-                Button = null;
-            }
-
-            if (CallButton != null) {
-                CallButton.Dispose ();
-                CallButton = null;
-            }
-
-            if (PhoneNumberText != null) {
-                PhoneNumberText.Dispose ();
-                PhoneNumberText = null;
-            }
-
-            if (TranslateButton != null) {
-                TranslateButton.Dispose ();
-                TranslateButton = null;
-            }
-        }
-    }
+		void ReleaseDesignerOutlets ()
+		{
+			if (Button != null) {
+				Button.Dispose ();
+				Button = null;
+			}
+			if (CallButton != null) {
+				CallButton.Dispose ();
+				CallButton = null;
+			}
+			if (PhoneNumberText != null) {
+				PhoneNumberText.Dispose ();
+				PhoneNumberText = null;
+			}
+			if (TranslateButton != null) {
+				TranslateButton.Dispose ();
+				TranslateButton = null;
+			}
+		}
+	}
 }
